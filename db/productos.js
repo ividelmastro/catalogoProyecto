b709f43460e7bd7d21a61f09d27db5489fced358
victoria <you@example.comsassoonvictoria@gmail.com>
2023-04-06 21:53:43 -0300

    modulo de datos

---
+++ db/productos.js
@@ -0,0 +1,120 @@
+let productps = {
+    lista: [
+        {
+            pieza: "Collar Mon Coeur Oro",
+            material: "Oro amarillo",
+            precio: 16.900,
+            descripcion: "Oro de 24 quilates: también conocido como oro puro, es el tipo de oro más puro que se puede encontrar en el mercado. Contiene un 99,9% de oro.",
+            imagen: "corazon con rojo adentro",
+            comentario: "Precioso, super elegante y clasico",
+        },
+        {
+            pieza: "Aros FIORE",
+            material: "Oro amarillo",
+            precio: 14.000,
+            descripcion: "Oro de 10 quilates: contiene un 41,7% de oro y un 58,3% de otros metales.",
+            imagen: "aros largos con rojo adentro",
+            comentario: "Una mezcla perfecta entre la finura y lo canchero",
+        },
+        {
+            pieza: "Collar inicial enchapado en oro",
+            material: "Oro amarillo",
+            precio: 15.000,
+            descripcion: "Oro de 14 quilates: contiene un 58,3% de oro y un 41,7% de otros metales.",
+            imagen: "collar con inicial A",
+            comentario: "Muy hermoso",
+        },
+        {
+            pieza: "Anillo Abbondanza",
+            material: "Bronce",
+            precio: 13.000,
+            descripcion: "Creamos este anillo de bronce que contiene la piedra Pirita conocida como la piedra de la abundancia.",
+            imagen: "anillo con dos piedras grises grandes",
+            comentario: "Elegante y autentico",
+        },
+        {
+            pieza: "Collar Chloe personalizado",
+            material: "Oro amarillo",
+            precio: 12.000,
+            descripcion: "Enchapado en Oro Amarillo",
+            imagen: "Collar Valen",
+            comentario: "Elegante y autentico",
+        },
+        {
+            pieza: "Pulsera Cleopatra",
+            material: "Oro amarillo con piedras azuladas",
+            precio: 14.000,
+            descripcion: "Enchapado en Oro Amarillo",
+            imagen: "pulsera con piedras azules",
+            comentario: "Increible esta pieza",
+        },
+        {
+            pieza: "Aros Ivanja",
+            material: "Oro amarillo",
+            precio: 13.500,
+            descripcion: "Baniados en Oro Amarillo",
+            imagen: "Aros argollas doradas",
+            comentario: "Clasicos y finos",
+        },
+        {
+            pieza: "Brazalete Cobalto",
+            material: "Oro amarillo y piedras de color Cobalto",
+            precio: 17.000,
+            descripcion: "Oro de 14 quilates: contiene un 58,3% de oro y un 41,7% de otros metales.",
+            imagen: "foto azul",
+            comentario: "Elegante y autentico",
+        },
+        {
+            pieza: "Anillo y pulsera Mercury",
+            material: "Oro amarillo",
+            precio: 14.000,
+            descripcion: "Oro de 18 quilates: contiene un 75% de oro y un 25% de otros metales como el cobre, la plata y el paladio.",
+            imagen: "mano con pulsera y anillos iguales",
+            comentario: "Son super comodos y estan muy de moda!",
+        },
+        {
+            pieza: "Collar Mali Baniado en Oro",
+            material: "Oro amarillo",
+            precio: 16.000,
+            descripcion: "Oro de 24 quilates: también conocido como oro puro, es el tipo de oro más puro que se puede encontrar en el mercado. Contiene un 99,9% de oro.",
+            imagen: "blazer blanco",
+            comentario: "Muy finos y cancheros!",
+        },
+
+
+    ],
+
+    usuarios: [
+        {
+            username: `Guillermina Lopez`,
+            email: `Glopez@gmail.com`,
+            birthDay: `17/03/2003`,
+            password: `documento`,
+            DNI: `44917243`,
+            image: "agregar"
+        },
+    ],
+
+    comentarios: [
+        {
+            nombre: `amante_de_joyas`,
+            texto: `Buenisima esta pieza, la recomiendo al 100%!`
+
+        },
+        {
+            nombre: `juana_sanchez`,
+            texto: `Piezas super autenticas y originales`
+        },
+        {
+            nombre: `Joya_lovers`,
+            texto: `Lugar super recomendado, piezas de excelente calidad y disenio!`
+        },
+        {
+            nombre: `fan_de_collares123`,
+            texto: `Me queria llevar todo! Muy buen trabajo y excelentes productos`
+        },
+
+    ]
+
+}
+module.exports = lista;
