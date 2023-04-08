63667c6572eb3885593de4024392efaa5dd9a4e7
victoria <you@example.comsassoonvictoria@gmail.com>
2023-04-08 20:29:21 -0300

    fotos index okk

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,9 @@
+var db = require("../db/productos");
+var listaDeProductos = db.listaDeProductos;
+
 var indexController = {
     index: function(req, res, next) {
-        res.render('index', { title: 'Mercado Liebre' });
+        res.render('index', { title: 'Mercado Liebre', lista:listaDeProductos });
     },
     productos: function(req, res, next) {
         res.render('product', { title: 'Producto' });
