56eaa16e8cefda1b01a31f5bf5eb1a60b89b0c20
Ivanka Del Mastro <ivanka@mbp-de-ivanka.lan>
2023-06-11 19:57:55 -0300

    instalaciones

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,19 +3,15 @@ const dbOrm = require('../database/models');
 var listaDeProductos = db.listaDeProductos;
 var usuario = db.usuario[0];
 const users = dbOrm.User;
+const bcrypt = require('bcryptjs');
 
 var usersController ={
     register: function(req, res, next) {
         res.render("register");
       },
     profile: function(req, res, next) {
-      users.findOne({
-        where: [{
-            id: 0
-        }],
-    })
     res.render("profile", {usuario, lista:listaDeProductos});
-      },
+    },
     login: function(req, res, next) {
         res.render("login");
       },
